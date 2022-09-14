ad2b92a29de16f43556648e9d2d3165d5b182a6c
dylanrattai <817682@mystma.org>
2022-09-14 15:08:24 -0500

    Goofy Ahh

---
+++ main.py
@@ -0,0 +1,13 @@
+def topFiveMovies(movieOne, movieTwo, movieThree, movieFour, movieFive):
+    print("Top 5 Movies")
+    print("1. " + movieOne)
+    print("2. " + movieTwo)
+    print("3. " + movieThree)
+    print("4. " + movieFour)
+    print("5. " + movieFive)
+
+def sayTimes(numInput, stringInput):
+    print((stringInput + " ") * numInput)
+
+topFiveMovies("Shrek", "Shrek 24", "Bee Movie", "Baby Driver", "Shrek 12")
+sayTimes(78346, "Goofy Ahhhhh")
